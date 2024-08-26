bafba74e97143916c760f6822f5f3f87661743b2
cryptomoveup <cryptomoveup@benfen.org>
2024-08-26 08:33:21 +0800

    fix links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -99,11 +99,11 @@ const config = {
             items: [
               {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                href: 'https://discord.gg/vDrgzyHjRq',
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/BenFen_Official',
               },
             ],
           },
@@ -116,12 +116,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/benfenorg/bfc',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} BenFen. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
