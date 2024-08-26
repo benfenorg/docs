3ab81a5bced568eb531a3530514db37c41096645
cryptomoveup <cryptomoveup@benfen.org>
2024-08-26 08:15:41 +0800

    fix button label

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/tutorial/start">
-            Docusaurus Tutorial - 5min ⏱️
+            BenFen Development Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
